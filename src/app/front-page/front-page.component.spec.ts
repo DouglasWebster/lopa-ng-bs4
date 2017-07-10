90fa5cf943dbfdec7aa365b561fcbeec810f65ec
Douglas Webster <douglas.webster2107@gmail.com>
2017-07-10 20:37:09 +0100

    Added fake user service to test

--- src/app/front-page/front-page.component.spec.ts
+++ src/app/front-page/front-page.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpModule } from '@angular/http';
 
+import { Observable } from 'rxjs/Observable';
+import { Subscriber} from 'rxjs/Subscriber';
 
 import { User } from '../shared/models';
 import { UserService, AlertService } from '../shared/services';
@@ -11,6 +13,16 @@ import { AlertComponent } from '../shared/directives/alert/alert.component';
 describe('FrontPageComponent', () => {
   let component: FrontPageComponent;
   let fixture: ComponentFixture<FrontPageComponent>;
+ 
+  const fakeUsers = '[{"firstName":"Micky","lastName":"Mouse","userName":"mMouse",'
+    + '"password":"Password1","admin":true,"id":1},{"firstName":"Daffy","lastName":"Duck",'
+    + '"userName":"dDuck","password":"Password2","admin":false,"id":2}]'
+
+  const userServiceStub = {
+    getAll(): Observable<String[]> {
+      return new Observable<String[]>((subscriber: Subscriber<String[]>) => subscriber.next(JSON.parse(fakeUsers)));
+    }
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,8 +35,8 @@ describe('FrontPageComponent', () => {
         AlertComponent
       ],
       providers: [
-        UserService,
-        AlertService
+        { provide: UserService, useValue: userServiceStub },
+          AlertService
       ]
     })
       .compileComponents();
